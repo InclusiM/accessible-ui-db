5af99bed6c070c2f175ddc3e64b746d0aeb08008
jnhugh <jnhugh23@colby.edu>
2025-03-06 10:30:41 -0500

    Update ServerConnection.js

--- ServerConnection.js
+++ ServerConnection.js
@@ -1,6 +1,6 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://jnhugh23:4kKTOM1oFyLc82KH@cluster0.kdegi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://jnhugh23:@cluster0.kdegi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
