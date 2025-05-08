f99d0ee87eb0bddd4aee0f16c4052b694e236540
Agnes <liaggie421@gmail.com>
2025-05-08 10:51:06 -0400

    update

--- client/src/UIService.js
+++ client/src/UIService.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const url = 'http://localhost:3000/api/UIscreenshots' || 'http://137.146.29.181:3011/api/UIscreenshots';
+const url = process.env.NODE_ENV === 'development' 
+? 'http://localhost:3000/api/UIscreenshots'
+: 'http://137.146.29.181:3011/api/UIscreenshots';
 
 class UIService {
     static async getUIScreenshots() {
