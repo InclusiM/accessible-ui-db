48a57f29e8a430b43d7d0499617fa767425c6b86
Agnes <liaggie421@gmail.com>
2025-05-08 09:50:11 -0400

    add permission

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 // Middleware
 // Configure CORS with specific options
 app.use(cors({
-  origin: ['http://localhost:8080'], 
+  origin: ['http://localhost:8080','http://137.146.29.181:8041'], 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true 
 }));
